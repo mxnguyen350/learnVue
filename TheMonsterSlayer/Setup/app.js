65e5a0fed355054667ecb4bf14a418a62c13de5d
mxnguyen350 <michael_nguyen@byu.edu>
2019-01-03 11:19:18 -0700

    finished monster slayer project

--- TheMonsterSlayer/Setup/app.js
+++ TheMonsterSlayer/Setup/app.js
@@ -5,12 +5,24 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         playerDamageDelt: 0,
-        monsterDamageDelt: 0
+        amountHealed: 0,
+        monsterDamageDelt: 0,
+        attackLog: []
     },
     methods: {
         // upon winning, give an alert with an option to restart or keep the screen as is
+        // use .push() to an array that will add the damage to the array, which then will be displayed to the log(list)
         startGame() {
             // Later, this should reset the health of each character to equal max
+            if (
+                this.playerHealth != 100 &&
+                this.monsterHealth != 100 &&
+                this.attackLog != []
+            ) {
+                this.playerHealth = 100;
+                this.monsterHealth = 100;
+                this.attackLog = [];
+            }
             return this.showMe = !this.showMe;
         },
         resetGame() {
@@ -18,15 +30,53 @@ new Vue({
         },
         attack() {
             // affect monster health at a certain rate
+            this.playerDamageDelt = (Math.floor(Math.random() * 14) + 1);
+            this.monsterHealth = this.monsterHealth - this.playerDamageDelt;
+            this.attackLog.unshift(`The monster took ${this.playerDamageDelt} damage`);
+            if (this.monsterHealth <= 0) {
+                alert('You won!')
+                return this.showMe = !this.showMe
+            }
             //damage yourself
+            this.monsterDamageDelt = (Math.floor(Math.random() * 19) + 1);
+            this.playerHealth = this.playerHealth - this.monsterDamageDelt;
+            this.attackLog.unshift(`The player took ${this.monsterDamageDelt} damage`);
+            if (this.playerHealth <= 0) {
+                alert('You lost!')
+                return this.showMe = !this.showMe
+            }
         },
         specialAttack() {
             // affect monster health at a higher rate
-            // damage yourself at the same rate as when you attack
+            this.playerDamageDelt = (Math.floor(Math.random() * 20) + 1);
+            this.monsterHealth = this.monsterHealth - this.playerDamageDelt;
+            this.attackLog.unshift(`The monster took ${this.playerDamageDelt} damage`);
+            if (this.monsterHealth <= 0) {
+                alert('You won!')
+                return this.showMe = !this.showMe
+            }
+            //damage yourself
+            this.monsterDamageDelt = (Math.floor(Math.random() * 19) + 1);
+            this.playerHealth = this.playerHealth - this.monsterDamageDelt;
+            this.attackLog.unshift(`The player took ${this.monsterDamageDelt} damage`);
+            if (this.playerHealth <= 0) {
+                alert('You lost!')
+                return this.showMe = !this.showMe
+            }
         },
         heal() {
             // do not affect monster health, heal yourself
+            this.amountHealed = (Math.floor(Math.random() * 20) + 1);
+            this.playerHealth = this.playerHealth + this.amountHealed;
+            this.attackLog.unshift(`The player healed himself for ${this.amountHealed}`);
             // then take damage
+            this.monsterDamageDelt = (Math.floor(Math.random() * 19) + 1);
+            this.playerHealth = this.playerHealth - this.monsterDamageDelt;
+            this.attackLog.unshift(`The player took ${this.monsterDamageDelt} damage`);
+            if (this.playerHealth <= 0) {
+                alert('You lost!')
+                return this.showMe = !this.showMe
+            }
         }
     }
 })
