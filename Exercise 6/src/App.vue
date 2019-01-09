90f2804ffc915916f5f029d947665c1996135b45
mxnguyen350 <michael_nguyen@byu.edu>
2019-01-09 16:04:13 -0700

    finished assignment 6 for vue

---
+++ Exercise 6/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <mxnguyen-complete></mxnguyen-complete>
+</template>
+
+<script>
+import Complete from './Home.vue';
+
+export default {
+    components: {
+        'mxnguyen-complete': Complete
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Exercise 6/src/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <part1-mxnguyen></part1-mxnguyen>
+        <hr>
+        <part2-mxnguyen></part2-mxnguyen>
+        <hr>
+        <part3-mxnguyen></part3-mxnguyen>
+    </div>
+</template>
+
+<script>
+import part1 from './Part1.vue';
+import part2 from './Part2.vue';
+import part3 from './Part3.vue';
+
+export default {
+    components: {
+        'part1-mxnguyen': part1,
+        'part2-mxnguyen': part2,
+        'part3-mxnguyen': part3
+    }
+}
+</script>
