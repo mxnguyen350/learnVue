5708913eb96122cc032d124e0c126b1f002600b2
mxnguyen350 <michael_nguyen@byu.edu>
2019-01-23 16:48:06 -0700

    intermediate commit

---
+++ module8/Section Code (Start)/src/components/UserDetail.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="component">
+        <h3>You may view the User Details here</h3>
+        <p>Many Details</p>
+        <p>UserName: {{ switchName() }}</p>
+        <button @click="resetName">Reset Name</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            name: {
+                type: String,
+                required: true
+            }
+        },
+        methods: {
+            switchName() {
+                return this.name.split("").reverse().join("");
+            },
+            resetName() {
+                this.name = 'Michael';
+                this.$emit('nameWasReset', this.name);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightcoral;
+    }
+</style>
